f100e19f500e2be980837c7d0fed3f4ff0f8283a
2Glitch27 <chiter1019@yandex.ru>
2026-02-02 12:12:49 +0300

    upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// let nick = prompt('Введите ваше имя');
+// alert( "Привет, " + nick );
+
+// let yearOfBirth = +prompt('Введите год своего рождения');
+// const ourYear = 2026;
+// alert(ourYear - yearOfBirth);
+
+// let squareLength = +prompt('Введите длину стороны квадрата');
+// alert (squareLength * 4);
+
+// let circleRadius = +prompt('Введите радиус окружности');
+// const PI = 3.14;
+// alert((circleRadius ** 2) * PI);
