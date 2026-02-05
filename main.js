28f78276d51995966c52778b917ddc2dbadcef40
2Glitch27 <chiter1019@yandex.ru>
2026-02-05 21:43:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,88 @@
 // let circleRadius = +prompt('Введите радиус окружности');
 // const PI = 3.14;
 // alert((circleRadius ** 2) * PI);
+
+
+
+
+
+
+
+
+
+// let age = +prompt("Введите возраст");
+// switch (true) {
+//     case age > 0 && age < 2 :
+//         alert ('Вы ребенок')
+//         break;
+//     case age > 12 && age <= 18 :
+//         alert ('Вы подросток')
+//         break;
+//     case age > 18 && age <= 60 :
+//         alert ('Вы взрослый')
+//         break;
+//     case age > 60 :
+//         alert ('Вы пенсионер') 
+//         break;
+//     default:
+//         alert ('Вы ошибка')
+//         break;
+// }
+
+
+// let symbol = +prompt('Введите число');
+// switch(symbol) {
+//     case 0 :
+//         alert (')')
+//         break;
+//     case 1 :
+//         alert ('!')
+//         break;
+//     case 2 :
+//         alert ('@')
+//         break;
+//     case 3 :
+//         alert ('#')
+//         break;
+//     case 4 :
+//         alert ('$')
+//         break;
+//     case 5 :
+//         alert ('%')
+//         break;
+//     case 6 :
+//         alert ('^')
+//         break;
+//     case 7 :
+//         alert ('&')
+//         break;
+//     case 8 :
+//         alert ('*')
+//         break;
+//     case 9 :
+//         alert ('(')
+//         break;
+// }
+
+// let num = +prompt('Введите трехзначное число');
+// let n100 = Math.floor(num / 100);
+// let n10 = (Math.floor(num / 10)) % 10;
+// let n1 = num % 10;
+// if (n1 === n10 && n10 === n100){
+//     alert ('Все одинаковые')
+// }else if (n1 === n10 || n1 === n100 || n10 === n100){
+//     alert('Две цифры')
+// } else{
+//     alert('Все разные')
+// }
+
+
+// let year = +prompt('Введите год');
+// if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0){
+//     alert('Високосный')
+// }else{
+//     alert('Невисокосный')
+// }
+
+
+
